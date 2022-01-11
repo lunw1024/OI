77809236ea140c94a1fb4b08168ef859eebd537e
idl0t <lunw1024@gmail.com>
2022-01-10 21:35:54 -0800

    2700 takedown

---
+++ codeforces/div2-761/e.cpp
@@ -0,0 +1,18 @@
+// 2700 takedown!
+// Key observation: the graph is a tree
+// construct the tree for all numbers, min # of operations == the distance between x and y
+// therefore answer is the diameter
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+
+int N;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    
+}
+
