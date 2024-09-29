efcf7fd6cec80716726ffbf388d3c6de84742fba
Alan Wu <lunw1024@gmail.com>
2024-09-29 19:00:18 +0000

    path dup

--- scripts/activate.fish
+++ scripts/activate.fish
@@ -2,11 +2,15 @@
 
 # Find the root git directory
 set root_dir (git rev-parse --show-toplevel)
+set script_dir $root_dir/scripts
 
-# Check if root_dir is non-empty
-if test -n "$root_dir"
+# Check if script_dir is non-empty
+if test -n "$script_dir"
     # Add root_dir/scripts/ to PATH
-    set -gx PATH $root_dir/scripts $PATH
+    if not contains $script_dir $PATH
+        set -gx PATH $script_dir $PATH
+    end
+
     echo "Activated OI env. `run`, `run-debug`, `run-with-aclib` now available."
 else
     echo "Not inside a Git repository."
